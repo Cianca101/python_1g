ce6b46b31bae3e2fa7719cfe028a25a37278e9e1
Cianca101 <emmanuel.carrillo.c@gmail.com>
2021-03-31 22:03:28 -0600

    Add Filters

--- dogtor/dogtor/settings.py
+++ dogtor/dogtor/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     "corsheaders",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
 
 ROOT_URLCONF = 'dogtor.urls'
